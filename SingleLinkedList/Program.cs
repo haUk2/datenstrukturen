f199d534e69265c839aed41057e16a8bac546550
haUk2 <91018531+haUk2@users.noreply.github.com>
2022-11-04 12:43:09 +0100

    s

--- SingleLinkedList/Program.cs
+++ SingleLinkedList/Program.cs
@@ -5,11 +5,7 @@ class Program
 {
     public static void Main(string[] args)
     {
-
-
-
         SingleLinkedListClass llist = new SingleLinkedListClass();
-
         llist.insertLast(1);
         llist.insertLast(2);
         llist.insertFirst(5);
@@ -17,9 +13,5 @@ class Program
         var node = llist.GetNode(6);
         llist.Delete(node);
         llist.printList();
-
-
-
-
     }
 }
